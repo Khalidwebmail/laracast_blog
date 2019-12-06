c387f9c8734c5182d6d12c568267b3bafae9442c
khalid <csewebmail@gmail.com>
2019-12-06 23:58:17 +0600

    20-Form query down to

--- resources/views/threads/partial/reply.blade.php
+++ resources/views/threads/partial/reply.blade.php
@@ -13,7 +13,10 @@
                 <form action="/replies/{{ $reply->id }}/favorites" method="POST">
                     {{ csrf_field() }}
                     <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
-                        {{ $reply->favourites()->count() }} {{ str_plural('Favorite', $reply->favourites()->count()) }}
+
+                        {{ $reply->favourites_count }} {{ str_plural('Favorite', $reply->favourites_count) }}
+
+                        {{-- {{ $reply->favourites()->count() }} {{ str_plural('Favorite', $reply->favourites()->count()) }} --}}
                     </button>
                 </form>
             </div>
